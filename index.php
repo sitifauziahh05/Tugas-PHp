a73ffb8e448a32d66e6c8f779b855d46bbdda563
SITI FAUZIAH <91424902+sitifauziahh05@users.noreply.github.com>
2022-04-08 23:32:22 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas Praktikum1</title>
+</head>
+<body style = "background-color:#E4CDA7;">
+<div id="header">
+    <h1><p align="center">Homepage Pribadi</p></h1>
+  </div>
+  <hr></hr>
+    <pre>
+        <?php
+   $Nama                  = "Siti Fauziah";
+   $NIM                   =  "6706210130";
+   $Kelas                 = "D3RPLA45-05";
+   $TTL   = "Tangerang/05 Mei 2003";
+   $Alamat                =  "KP. Kalibaru, RT/RW 04/04, Kec. Balaraja, Kab. Tangerang";
+   $RiwayatPendidikan     =" ";
+        $SD                 = "SDN Tobat 4";
+        $SMP                = "SMPQ Insan Pratama";
+        $SMA                = "MAN 2 Tangerang";
+   $Email                 = "sitifauziah@student.telkomuniversity.ac.id";
+   $Instagram             = "stfzh.05";
+   $Hobby                 = "makan, nonton dan membaca";
+   $DeskripsiPribadiSaya = "saya sekarang sedang berkuliah di telkom university, Fakultas ilmu terapa, Jurusan Rekayasa Perangkat Lunak Aplikasi. 
+                                 saya suka kuliner maka dari itu saya mempunyai cita-cita membangun Mall.";
+    
+
+    echo"1.Nama                 : $Nama<br>";
+    echo"        2.NIM                  : $NIM<br>";
+    echo"        3.Kelas                : $Kelas<br>";
+    echo"        4.TTL                  : $TTL<br>";
+    echo"        5.Alamat               : $Alamat<br>";
+    echo"        6.RiwayatPendidikan    : $RiwayatPendidikan<br>";
+    echo"            SD                      : $SD<br>";
+    echo"            SMP                     : $SMP<br>";
+    echo"            SMA                     : $SMA<br>";
+    echo"       7.Email                 : $Email<br>";
+    echo"       8.Instagram             : $Instagram<br>";
+    echo"       9.Hobby                 : $Hobby<br>";
+    echo"      10.DeskripsiPribadiSaya  : $DeskripsiPribadiSaya<br>";
+    
+   ?>
+</body>
+</html>
